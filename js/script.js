6dea19537f518b56e11b8dc293b106160ac9b9f6
Sarthak Chatterjee <sarthatc@gmail.com>
2023-07-03 03:50:01 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,91 @@
+//alert("Feeling OK?");
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => { // it can be onclick also
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginBtn.classList.remove('active');
+    navBar.classList.remove('active');
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () => { 
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginBtn.classList.remove('active');
+    navBar.classList.remove('active');
+}
+
+let loginBtn = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => { 
+    loginBtn.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navBar.classList.remove('active');
+}
+
+let navBar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => { 
+    navBar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginBtn.classList.remove('active');
+}
+
+window.onscroll = () => {
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginBtn.classList.remove('active');
+    navBar.classList.remove('active');
+}
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+    },
+    clearedSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+  var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+    },
+    clearedSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+
+
